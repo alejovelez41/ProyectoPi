673ec05d517397773f47b5b26af6b699e55d5d0a
alejo <alejovelez41>
2020-05-09 11:10:59 +0200

    Base de datos añadida

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,6 @@
+import pandas as pd
 #############################################################################
 print("Hello World")
 print("Ensayo 2")
+CovCol=pd.read_csv('CovCol.csv', header='infer')  
+CovCol.head()
