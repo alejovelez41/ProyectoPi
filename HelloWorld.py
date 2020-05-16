4e0a1cf311693361853a069fd250b952b8e5a486
andres22766 <aescuderol@unal.edu.co>
2020-05-15 22:29:39 -0500

    intento 1 de grafica

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt #biblioteca para graficar
+import seaborn as sns #biblioteca para graficar
 #############################################################################
 print("Hello World")
 print("Ensayo 2")
@@ -7,5 +9,8 @@ datos=pd.read_csv("CovidCol.csv",header=0)
 print (datos)
 print("_-----_")
 print(datos.head())
-
-
+df=pd.DataFrame(datos)
+df.groupby("atención")["Edad"].mean().plot(kind="bar",legend="reverse")
+df.columns.values
+#print(df.describe(datos)) #esto muestra estad basicas
+print (df.columns.values)
